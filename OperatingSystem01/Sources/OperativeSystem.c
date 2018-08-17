c727f79c9353f1cd7b227a67b3af386f2d648e9d
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-08-17 16:28:04 -0500

    add more task functions

--- OperatingSystem01/Sources/OperativeSystem.c
+++ OperatingSystem01/Sources/OperativeSystem.c
@@ -8,56 +8,101 @@
 #include "User.h"
 #include "OperativeSystem.h"
 #include "Queue.h"
+#define __ASM __asm /*!< asm keyword for GNU Compiler */ 
+#define __INLINE inline /*!< inline keyword for GNU Compiler */ 
+#define __STATIC_INLINE static inline 
+extern int set_lr(uint32_t a);
 
-TASK first;
+/** 
+ \brief Get Link Register 
+ \details Returns the current value of the Link Register (LR). 
+ \return LR Register value 
+ */
+__attribute__( ( always_inline ))  __STATIC_INLINE uint32_t __get_LR(void) {
+	register uint32_t result;
+
+	__ASM volatile ("MOV %0, LR\n" : "=r" (result) );
+	return (result);
+}
+
+__attribute__( ( always_inline ))  __STATIC_INLINE void __set_LR(int value) {
+	__ASM volatile ("MOV LR, [LR]");
+}
+
+TASK runningTask;
+TASK nextTask;
 TASK ready_arr[20];
+uint32_t a;
 
 QueueType readyQueue = { 0, 0, STATIC_ALLOC, { } };
+QueueType *ready_queue;
 
-void ActivateTask(TASK task){
-	
+void ActivateTask(TASK task) {
+	task.state = READY;
+	Queue_Add(ready_queue, task);
+	CheckNextTask();
+	RunNextTask();
 }
 
-void ChainTask(TASK task){
-	
-}
-
-void TerminateTask(){
-	
-}
-
-void OS_init(volatile TASK *tasks, int size){
-	checkAutoStartSetReady(tasks, size);
-	TASK firstTask;
-	firstTask = checkHighestPriority(ready_arr, size);
-	firstTask.ap_task_init();
-}
-
-void checkAutoStartSetReady(TASK *tasks, int size){
-	int count = 0;
-		int i;
-		for(i=0;i<size;i++){
-			if(tasks[i].autostart == 1){
-				tasks[i].state = READY; //si tiene autostart la tarea se va a ready
-				tasks[i].autostart = 0;
-			}	
-			if(tasks[i].state == READY){
-				ready_arr[count] = tasks[i];
-				count++;
-			} 
+void RunNextTask(){
+	if (nextTask.priority > runningTask.priority) {
+			runningTask.return_direction = a;
+			runningTask = nextTask;
+			RunTask();
 		}
 }
 
-TASK checkHighestPriority(TASK *tasks, int size){
+void ChainTask(TASK task) {
+	Queue_Add(ready_queue, task);
+	TerminateTask();
+	ActivateTask(task);
+}
+
+void TerminateTask(void) {
+	runningTask.state = IDLE;
+	runningTask = get_empty_TASK();
+	if(CheckNextTask()){
+		RunTask();
+	}	
+}
+
+int CheckNextTask(void){
+	if(TaskList_Empty(ready_queue)){
+		return 0;
+	}
+	Sort_TaskList(ready_queue);
+	nextTask = Queue_Next(ready_queue);
+	return 1;
+}
+
+void RunTask(void) {
+	runningTask.state = RUNNING;
+	if (runningTask.return_direction != 0) {
+		set_lr(runningTask.return_direction);
+	} else {
+ 		runningTask.ap_task_init();
+	}
+}
+
+void OS_init(TASK *tasks, int size) {
+	ready_queue = &readyQueue;
+	checkAutoStartSetReady(tasks, size);
+	Sort_TaskList(ready_queue);
+	runningTask = Queue_Next(ready_queue);
+	RunTask();
+}
+
+void checkAutoStartSetReady(TASK *tasks, int size) {
+	int count = 0;
 	int i;
-	TASK highest = tasks[0];
-	for(i = 1; i<=3; i++){
-		if(tasks[i].priority > highest.priority){
-			highest = tasks[i];
+	for (i = 0; i < size; i++) {
+		if (tasks[i].autostart == 1) {
+			tasks[i].state = READY; //si tiene autostart la tarea se va a ready
+			tasks[i].autostart = 0;
+		}
+		if (tasks[i].state == READY) {
+			Queue_Add(ready_queue, tasks[i]);
+			count++;
 		}
 	}
-	return highest;
 }
-
-
-
