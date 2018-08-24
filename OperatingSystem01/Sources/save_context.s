55c8b9a0d206d9e1c8d379e90453a87f406fdf8e
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-08-24 17:40:35 -0500

    adding functions for getting global registers

---
+++ OperatingSystem01/Sources/save_context.s
@@ -0,0 +1,41 @@
+@@@@@@@@@@@@@@@@@@@@
+.globl   get_r0
+.p2align 2
+.type    get_r0,%function
+		
+get_r0:                    
+	.fnstart
+	add 	r0, r0, #0
+	bx      lr
+	.fnend
+@@@@@@@@@@@@@@@@@@@@
+.globl   get_r1
+.p2align 2
+.type    get_r1,%function
+		
+get_r1:                    
+	.fnstart
+	add 	r0, r1, #0
+	bx      lr
+	.fnend
+@@@@@@@@@@@@@@@@@@@@
+.globl   get_r2
+.p2align 2
+.type    get_r2,%function
+		
+get_r2:                    
+	.fnstart
+	add 	r0, r2, #0
+	bx      lr
+	.fnend
+@@@@@@@@@@@@@@@@@@@@
+.globl   get_r3
+.p2align 2
+.type    get_r3,%function
+		
+get_r3:                    
+	.fnstart
+	add 	r0, r3, #0
+	bx      lr
+	.fnend
+@@@@@@@@@@@@@@@@@@@@
