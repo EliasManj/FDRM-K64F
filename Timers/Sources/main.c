14e73de88d36f5a82600ec493e222740ea25c459
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-10-19 14:26:33 -0500

    pll

--- Timers/Sources/main.c
+++ Timers/Sources/main.c
@@ -49,7 +49,6 @@ int main(void) {
 	return 0;
 }
 
-
 void fll_Init(void) {
 	//SIM_SCGC6|=(1<<27)+(1<<24);
 	SIM_SCGC6 |= (1 << 24);
@@ -59,12 +58,12 @@ void fll_Init(void) {
 	//SIM_SOPT2 |= (3 << 24);
 	PORTD_PCR1 =1<<8;
 	GPIOD_PDDR = 0x00000002;
-	MCG_S |= (1 << 4)+1;
+	MCG_S |= (1 << 4) + 1;
 	MCG_C6 &= ~(1 << 6);	//PLLS
 	//MCG_C4 |= (1 << 7);	//DMX32
 	//MCG_C4 |= (3 << 5);	//DRS
 	//MCG_C1 |= (1<<6);		//CLKS
-	MCG_C1 |= 1 << 1;		
+	MCG_C1 |= 1 << 1;
 	MCG_C1 |= (1 << 2);		//IREFS
 	MCG_C2 |= 1;
 	//MCG_SC |= (5<<1);
@@ -77,4 +76,52 @@ void fll_Init(void) {
 
 	PORTE_PCR29 =3<<8; //TPM0_CH2
 
+}
+
+
+void pll_Init(void){
+
+	MCG_C1 &= (0 << 7)+(0 << 6)+(0<<2);	//IREFS
+	
+	MCG_C2 |= (1 << 2);		//EREFS OSCILLATOR
+	MCG_C2 |= (1 << 4);   // OSC LowFreq 0 HighFreq 1
+	
+	MCG_C5 |= 14;	//PRDIV0 =  /24 este divide 
+	//MCG_C5 |= (1<<6);	//PLL CLOCK ENABLE
+	//MCG_C5 |= 1;		//PLL CLOCK ENABLE
+
+	MCG_C6 |= (1 << 6);	//PLLS
+	MCG_C6 |= 31;	// VCO DIVIDER 55 este multiplica
+		
+	//MCG_C7 |= (1<<1);	//OSCSEL = 01 32Khz OSC1 = 42Khz
+	
+	MCG_S |= (1 << 4);
+	MCG_S |= (1 << 3) +(1 << 2); // OUTPUT OF PLL IS SELECTED
+	
+	SIM_SCGC6 |= (1 << 24);
+	SIM_SCGC5 |= (1 << 13) + (1 << 12);	//Port E
+
+	PORTD_PCR1 =1<<8;
+	GPIOD_PDDR = 0x00000002;
+	
+	FTM0_SC |= 1 << 3; //iniciar conteo
+	FTM0_C2SC |= (1 << 6) + (5 << 2); //hab interrupciones
+
+	NVIC_ICPR(1) = (1 << (42%32));  //Banderas TPM0
+	NVIC_ISER(1) = (1 << (42%32));
+
+	PORTE_PCR29 =3<<8; //TPM0_CH2
+}
+
+
+void setGPIO_PortC(void) {
+	SIM_SCGC5 |= (1 << 11);	//Activate clock of port C
+	PORTC_PCR5 = (1<<8);	//Set PTC5 as GPIO
+	PORTC_PCR0 = (1<<8);	//Set PTC0 as GPIO
+	PORTC_PCR8 = (1<<8);	//Set PTC8 as GPIO
+	PORTC_PCR3 = (1<<8);	//Set PTC3 as GPIO
+	GPIOC_PDDR |= (1 << 5);	//Set PTC5 as output
+	GPIOC_PDDR |= (1 << 0);	//Set PTC0 as output
+	GPIOC_PDDR |= (1 << 8);	//Set PTC8 as output
+	GPIOC_PDDR |= (1 << 3);	//Set PTC3 as output
 }
